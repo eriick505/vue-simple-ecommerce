b32e087c576c2c354afe3a6f031cc7171bc86bc4
Erick <santoserick9@gmail.com>
2022-04-05 13:31:31 -0300

    style: add responsive width

--- src/layouts/LoginContent.vue
+++ src/layouts/LoginContent.vue
@@ -5,11 +5,11 @@ import SignInForm from "./SignInForm.vue";
 <template>
   <section class="bg-gray-900 h-full py-4 px-6 flex flex-col justify-center">
     <header class="mb-5">
-      <h2 class="text-5xl font-bold text-white">
+      <h2 class="text-4xl md:text-5xl font-bold text-white">
         Sign in to CRUD<span class="text-blue-500">.</span>
       </h2>
     </header>
-    <div class="max-w-[40%]">
+    <div class="md:max-w-[40%]">
       <SignInForm />
     </div>
   </section>

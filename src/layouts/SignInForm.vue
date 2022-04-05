88cde3e5acfcc1c9464c13342759b9d923cd9dd9
Erick <santoserick9@gmail.com>
2022-04-05 13:32:34 -0300

    feat: call action from authStore when submit is triggered

--- src/layouts/SignInForm.vue
+++ src/layouts/SignInForm.vue
@@ -1,17 +1,34 @@
 <script setup lang="ts">
 import { ref } from "vue";
+import { useAuthStore } from "@/stores/auth";
+
 import BaseInputText from "@/components/BaseInputText.vue";
 import IconEmail from "@/components/icons/IconEmail.vue";
 import IconPadlock from "@/components/icons/IconPadlock.vue";
 
-const email = ref("");
-const password = ref("");
+import type { AuthLoginRequest } from "@/types";
+
+const authStore = useAuthStore();
+
+const login = ref<AuthLoginRequest>({
+  email: "",
+  password: "",
+});
+
+const handleSubmit = async () => {
+  await authStore.loginUser(login.value);
+};
 </script>
 
 <template>
-  <form>
+  <form @submit.prevent="handleSubmit">
     <div class="mb-4">
-      <BaseInputText v-model="email" required placeholder="Email" type="text">
+      <BaseInputText
+        v-model="login.email"
+        required
+        placeholder="Email"
+        type="email"
+      >
         <template #icon>
           <IconEmail
             class="absolute top-[50%] right-3 text-xl z-50 -translate-y-[50%] text-gray-200"
@@ -22,7 +39,7 @@ const password = ref("");
 
     <div class="mb-5">
       <BaseInputText
-        v-model="password"
+        v-model="login.password"
         required
         placeholder="Password"
         type="password"
@@ -39,7 +56,7 @@ const password = ref("");
       <button
         class="bg-blue-500 text-white px-16 py-3 rounded-full hover:bg-blue-800 transition-all"
       >
-        SIGN IN
+        {{ authStore.loading ? "CARREGANDO" : "SIGN IN" }}
       </button>
     </div>
   </form>

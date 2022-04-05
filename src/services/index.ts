b807b03d54f8675f63cd1cc6d75b8fbeba398eff
Erick <santoserick9@gmail.com>
2022-04-05 13:32:49 -0300

    feat: add interception to get token from local storage

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,4 +1,5 @@
 import axios, { type AxiosResponse } from "axios";
+import { TOKEN_KEY } from "@/utils/localStorage";
 
 export type HttpResponseData<T> = AxiosResponse<T>;
 
@@ -6,4 +7,15 @@ const baseUrl = import.meta.env.VITE_API_ENDPOINT;
 
 export const http = axios.create({
   baseURL: `${baseUrl}`,
+});
+
+http.interceptors.request.use((config) => {
+  const token = window.localStorage.getItem(TOKEN_KEY);
+
+  if (token) {
+    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+    config.headers!["Authorization"] = `bearer ${token}`;
+  }
+
+  return config;
 });

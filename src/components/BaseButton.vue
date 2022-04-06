bf30458465aecbb47c3d8023e8e4fae87c6f35b8
Erick <santoserick9@gmail.com>
2022-04-06 15:15:05 -0300

    feat: create base button

---
+++ src/components/BaseButton.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+interface ITheBaseButton {
+  text: string;
+}
+
+defineProps<ITheBaseButton>();
+</script>
+
+<template>
+  <button
+    class="bg-blue-500 text-white px-16 py-3 rounded-full hover:bg-blue-800 transition-all"
+  >
+    {{ text }}
+  </button>
+</template>
+
+<style scoped></style>

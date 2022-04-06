37013faa63d68d2507209b445b93ef9ac0de2f91
Erick <santoserick9@gmail.com>
2022-04-06 17:35:37 -0300

    chore: router to test

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,15 @@
+<script setup lang="ts">
+import { RouterLink } from "vue-router";
+</script>
+
 <template>
   <div class="about">
+    <nav class="my-5 flex gap-10">
+      <RouterLink to="/" class="hover:text-blue-500">Go To Home </RouterLink>
+      <RouterLink to="/about" class="hover:text-blue-500">
+        Go To About
+      </RouterLink>
+    </nav>
     <h1>This is an about page</h1>
   </div>
 </template>

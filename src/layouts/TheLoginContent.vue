9985348ebe5b7142f857041333eddde44aa16a81
Erick <santoserick9@gmail.com>
2022-04-07 10:50:23 -0300

    feat: add props to change text template by type

--- src/layouts/TheLoginContent.vue
+++ src/layouts/TheLoginContent.vue
@@ -1,17 +1,33 @@
 <script setup lang="ts">
+import { RouterLink } from "vue-router";
+
 interface ITheLoginContentProps {
   title: string;
+  type: "signin" | "signup";
+  linkTakes: "login" | "register";
 }
 
-defineProps<ITheLoginContentProps>();
+const props = defineProps<ITheLoginContentProps>();
+
+const changeCTA =
+  props.type === "signin"
+    ? "Don't have an account yet?"
+    : "Already an account?";
 </script>
 
 <template>
-  <section class="bg-gray-900 h-full py-4 px-6 flex flex-col justify-center">
+  <section class="h-full py-4 px-6 flex flex-col justify-center">
     <header class="mb-5">
+      <span class="block mb-4 text-blue-500 font-medium">START FOR FREE</span>
       <h2 class="text-4xl md:text-5xl font-bold text-white">
         {{ title }}<span class="text-blue-500">.</span>
       </h2>
+      <p class="text-white mt-6 mb-3">
+        {{ changeCTA }}
+        <RouterLink :to="linkTakes" class="text-blue-500 capitalize">
+          {{ linkTakes }}
+        </RouterLink>
+      </p>
     </header>
     <div class="md:max-w-[40%]">
       <slot name="form"></slot>

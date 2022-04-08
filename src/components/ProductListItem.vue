4a27e1069ec4aa9fe98b72c676c84693762fb785
Erick <santoserick9@gmail.com>
2022-04-08 18:47:43 -0300

    feat: get product props

--- src/components/ProductListItem.vue
+++ src/components/ProductListItem.vue
@@ -1,22 +1,34 @@
+<script setup lang="ts">
+import { getServerImage } from "@/utils/getServerImage";
+import { formatterPrice } from "@/utils/formatterPriceBRL";
+import type { IProduct } from "@/types";
+
+interface IProductListItemProps {
+  product: IProduct;
+}
+
+const props = defineProps<IProductListItemProps>();
+
+const productImage = getServerImage(props.product.image_product.path);
+const productPrice = formatterPrice(Number(props.product.price));
+</script>
+
 <template>
   <article
     class="bg-gray-900 shadow-lg shadow-indigo-500/10 rounded-md overflow-hidden"
   >
     <header class="flex justify-center items-center">
-      <img
-        src="https://images.unsplash.com/photo-1557180295-76eee20ae8aa?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80"
-        alt="image"
-      />
+      <img :src="productImage" :alt="product.name" />
     </header>
     <section class="p-5">
       <header class="pb-2">
-        <h2>Phone Retro Yellow</h2>
+        <h2>{{ product.name }}</h2>
       </header>
       <footer
         class="flex justify-between items-center pt-3 border-t-[1px] border-slate-800"
       >
         <div>
-          <span class="font-bold">$299.95</span>
+          <span class="font-bold">{{ productPrice }}</span>
         </div>
         <div>
           <button class="mr-3">EDIT</button>

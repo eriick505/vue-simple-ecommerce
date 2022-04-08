a33c740d7b4909f9a20fa085c9431babbf57294e
Erick <santoserick9@gmail.com>
2022-04-08 16:49:43 -0300

    feat: add list product component

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,24 +1,10 @@
 <script setup lang="ts">
-import { RouterLink } from "vue-router";
-import TheWelcome from "@/components/TheWelcome.vue";
+import ListProduct from "@/components/ListProduct.vue";
 </script>
 
 <template>
-  <main>
-    <nav class="my-5 flex gap-10">
-      <RouterLink to="/" class="hover:text-blue-500">Go To Home </RouterLink>
-      <RouterLink to="/about" class="hover:text-blue-500">
-        Go To About
-      </RouterLink>
-    </nav>
-    <TheWelcome />
-    <p>
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magni veritatis
-      harum iste eaque nulla quaerat doloribus iure rerum culpa unde?
-      Voluptatem, quibusdam laboriosam, quis mollitia quae expedita inventore
-      iusto, reprehenderit tempora earum voluptatibus. Dicta ratione blanditiis
-      facere quia placeat ullam fugit commodi facilis, suscipit porro optio
-      numquam id? Neque, qui.
-    </p>
+  <main class="text-white">
+    <h2>LISTA DE PRODUTOS</h2>
+    <ListProduct />
   </main>
 </template>

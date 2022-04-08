bcaf11e5346ee9458c33abf469c1a6ba2f2055a5
Erick <santoserick9@gmail.com>
2022-04-08 18:49:41 -0300

    feat: fetch product list

--- src/components/ProductList.vue
+++ src/components/ProductList.vue
@@ -1,14 +1,27 @@
 <script setup lang="ts">
+import { onMounted } from "vue";
+import { useProductStore } from "@/stores/products";
 import ProductListItem from "@/components/ProductListItem.vue";
-import { ref } from "vue";
 
-const list = ref([1, 2, 3]);
+const productStore = useProductStore();
+
+onMounted(() => {
+  productStore.getProductList();
+});
 </script>
 
 <template>
-  <ul class="grid md:grid-cols-3 gap-3">
-    <li v-for="item in list" :key="item">
-      <ProductListItem />
-    </li>
-  </ul>
+  <section>
+    <h2 class="text-3xl text-white text-center font-bold mb-7">PRODUCT LIST</h2>
+
+    <h2 class="text-3xl text-white" v-show="productStore.loading">
+      Loading...
+    </h2>
+
+    <ul class="grid md:grid-cols-3 gap-4" v-show="!productStore.loading">
+      <li v-for="product in productStore.productList" :key="product.id_product">
+        <ProductListItem :product="product" />
+      </li>
+    </ul>
+  </section>
 </template>

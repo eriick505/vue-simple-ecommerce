40aa5dbc0e1223f34e0379428ae021869bfcc441
Erick <santoserick9@gmail.com>
2022-04-08 16:49:14 -0300

    feat: create list and list item from products

---
+++ src/components/ListProduct.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import ListProductItem from "@/components/ListProductItem.vue";
+import { ref } from "vue";
+
+const list = ref([1, 2, 3]);
+</script>
+
+<template>
+  <ul class="grid md:grid-cols-3 gap-3">
+    <li v-for="item in list" :key="item">
+      <ListProductItem />
+    </li>
+  </ul>
+</template>
---
+++ src/components/ListProductItem.vue
@@ -0,0 +1,28 @@
+<template>
+  <article
+    class="bg-gray-900 shadow-lg shadow-indigo-500/10 rounded-md overflow-hidden"
+  >
+    <header class="flex justify-center items-center">
+      <img
+        src="https://images.unsplash.com/photo-1557180295-76eee20ae8aa?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80"
+        alt="image"
+      />
+    </header>
+    <section class="p-5">
+      <header class="pb-2">
+        <h2>Phone Retro Yellow</h2>
+      </header>
+      <footer
+        class="flex justify-between items-center pt-3 border-t-[1px] border-slate-800"
+      >
+        <div>
+          <span class="font-bold">$299.95</span>
+        </div>
+        <div>
+          <button class="mr-3">EDIT</button>
+          <button>CART</button>
+        </div>
+      </footer>
+    </section>
+  </article>
+</template>

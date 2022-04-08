c09982e2be102f422421cd8d8bafc6fd22ea25ad
Erick <santoserick9@gmail.com>
2022-04-08 18:48:57 -0300

    Revert "feat: featch product list"
    This reverts commit 0a86ef04a470413efd496d7fa795f6fa01153f3e.

--- src/components/ProductList.vue
+++ src/components/ProductList.vue
@@ -1,27 +1,14 @@
 <script setup lang="ts">
-import { onMounted } from "vue";
-import { useProductStore } from "@/stores/products";
 import ProductListItem from "@/components/ProductListItem.vue";
+import { ref } from "vue";
 
-const productStore = useProductStore();
-
-onMounted(() => {
-  productStore.getProductList();
-});
+const list = ref([1, 2, 3]);
 </script>
 
 <template>
-  <section>
-    <h2 class="text-3xl text-white text-center font-bold mb-7">PRODUCT LIST</h2>
-
-    <h2 class="text-3xl text-white" v-show="productStore.loading">
-      Loading...
-    </h2>
-
-    <ul class="grid md:grid-cols-3 gap-4" v-show="!productStore.loading">
-      <li v-for="product in productStore.productList" :key="product.id_product">
-        <ProductListItem :product="product" />
-      </li>
-    </ul>
-  </section>
+  <ul class="grid md:grid-cols-3 gap-3">
+    <li v-for="item in list" :key="item">
+      <ProductListItem />
+    </li>
+  </ul>
 </template>

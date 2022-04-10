7c0e6104d9fde3ee4f87cce06e131d51c29712a8
Erick <santoserick9@gmail.com>
2022-04-10 17:55:07 -0300

    feat: add router view component

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
+import { RouterView } from "vue-router";
 import ProductList from "@/components/ProductList.vue";
 </script>
 
 <template>
   <main class="text-white">
+    <RouterView />
     <ProductList />
   </main>
 </template>

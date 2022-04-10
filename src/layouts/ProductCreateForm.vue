2460990ffecc0918447b63c641e41700aa793321
Erick <santoserick9@gmail.com>
2022-04-10 18:13:34 -0300

    feat: add product create form component and add slot in the modal component

---
+++ src/layouts/ProductCreateForm.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import TheModal from "@/layouts/TheModal.vue";
+</script>
+
+<template>
+  <TheModal>
+    <template #modalContent>
+      <div>
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Suscipit,
+          fugit beatae. Alias, nihil nobis reprehenderit eos ipsum harum minima.
+          Numquam alias molestias velit necessitatibus illo. Expedita qui
+          quibusdam ex aliquid. Autem corporis iusto velit! Sequi id minima
+          aliquid asperiores eligendi modi nemo aut doloribus ex. Reiciendis
+          atque dolore doloremque voluptas.
+        </p>
+      </div>
+    </template>
+  </TheModal>
+</template>
+
+<style scoped></style>

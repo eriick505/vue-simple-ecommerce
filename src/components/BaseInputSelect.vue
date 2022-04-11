5463a8318dcb558df44a5546b7f5c8916a23676a
Erick <santoserick9@gmail.com>
2022-04-11 10:40:54 -0300

    feat: add emits on select change to bind with modelValue passing by v-model prop

--- src/components/BaseInputSelect.vue
+++ src/components/BaseInputSelect.vue
@@ -1,37 +1,64 @@
 <script setup lang="ts">
-import { ref } from "vue";
+import { ref, useAttrs, type SelectHTMLAttributes } from "vue";
 import IconChevron from "./icons/IconChevron.vue";
 
-const customArrow = ref(false);
+interface IBaseInputSelectProps extends SelectHTMLAttributes {
+  modelValue: string;
+}
 
-const toggleCustomArrow = () => {
-  customArrow.value = !customArrow.value;
+interface IBaseInputSelectEmits {
+  (e: "update:modelValue", value: string): void;
+}
+
+defineProps<IBaseInputSelectProps>();
+const emits = defineEmits<IBaseInputSelectEmits>();
+const attrs = useAttrs();
+
+const customArrow = ref(false);
+const selected = ref("");
+
+const toggleCustomArrow = () => (customArrow.value = !customArrow.value);
+
+const onSelectChange = (e: Event) => {
+  const valueSelected = (e.target as HTMLInputElement).value;
+  emits("update:modelValue", valueSelected);
 };
 </script>
 
 <template>
   <div class="relative">
-    <span
-      :class="{ 'rotate-180': customArrow }"
-      class="absolute top-1/2 -translate-y-1/2 right-2 text-white transition-all text-2xl rotate-0 pointer-events-none"
-    >
+    {{ selected }}
+    <span :class="{ active: customArrow }" class="baseInputSelectCustomArrow">
       <IconChevron />
     </span>
     <select
       @click="toggleCustomArrow"
       @blur="toggleCustomArrow"
+      @change="onSelectChange"
+      :value="modelValue"
+      v-bind="attrs"
       class="p-4 w-full rounded-lg bg-gray-800 text-white appearance-none"
     >
+      <option disabled value="">Please select one</option>
+      <option value="1">Category 2</option>
       <option value="2">Category 2</option>
+      <option value="hfghf">Category 2</option>
+      <option value="asd">Category 2</option>
       <option value="2">Category 2</option>
+      <option value="43">Category 2</option>
       <option value="2">Category 2</option>
-      <option value="2">Category 2</option>
-      <option value="2">Category 2</option>
-      <option value="2">Category 2</option>
-      <option value="2">Category 2</option>
-      <option value="2">Category 2</option>
-      <option value="2">Category 2</option>
-      <option value="2">Category 2</option>
+      <option value="nbnb">Category 2</option>
+      <option value="sdfs">Category 2</option>
+      <option value="334">Category 2</option>
     </select>
   </div>
 </template>
+
+<style scoped lang="postcss">
+.baseInputSelectCustomArrow {
+  @apply absolute top-1/2 -translate-y-1/2 right-2 text-white transition-all text-2xl rotate-0 pointer-events-none;
+}
+.baseInputSelectCustomArrow.active {
+  @apply rotate-180;
+}
+</style>
